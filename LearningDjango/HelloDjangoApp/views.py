7049a3d6bf7e77afb19426dc17974819b04c3f80
AI-Ned <67223760+AI-Ned@users.noreply.github.com>
2020-07-03 00:25:39 +0100

    Added and learned how views.py interacts with the html templates
    I also learnt how to add an app to the setting.py so it is not ignored when the site it ran.  -- It also allows for the site to be rendered.

--- LearningDjango/HelloDjangoApp/views.py
+++ LearningDjango/HelloDjangoApp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    # The render funtion pulls through the HTML template, it also allows for a dictionary to populate variables outlined on the HTML template. 
+    return render(
+        request, 
+        "HelloDjangoApp/index.html",
+            {
+                'title':"Hello Django!",
+                'message':"Hello World",
+                'content':" on " + now.strftime("%A, %d %B, %Y, at %X")
+            }
+        )
